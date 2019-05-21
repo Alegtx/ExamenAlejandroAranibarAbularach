0b846d2893b6c5b8b7d4089d018b49ccf59e9693
universitario <hub@gmail.com>
2019-05-21 18:41:52 -0400

    Test3

--- app/src/main/java/alegtx/univalle/com/examen/Main.java
+++ app/src/main/java/alegtx/univalle/com/examen/Main.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Main extends AppCompatActivity {
-    public String TipoConversion="";
+    public String TipoConversion="",Conversion="";
     public Button CF,FC,CK,KC,MC,CM,CP,PC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +19,55 @@ public class Main extends AppCompatActivity {
     public void C_F(View v){
         CF=(Button)findViewById(R.id.btn_C_F);
         TipoConversion=CF.getText().toString();
+        Conversion="1";
+        CambiarVentana();
+    }
+    public void F_C(View v){
+        FC=(Button)findViewById(R.id.btn_F_C);
+        TipoConversion=FC.getText().toString();
+        Conversion="2";
+        CambiarVentana();
+    }
+    public void C_K(View v){
+        CK=(Button)findViewById(R.id.btn_C_K);
+        TipoConversion=CK.getText().toString();
+        Conversion="3";
+        CambiarVentana();
+    }
+    public void K_C(View v){
+        KC=(Button)findViewById(R.id.btn_K_C);
+        TipoConversion=KC.getText().toString();
+        Conversion="4";
+        CambiarVentana();
+    }
+    public void M_C(View v){
+        MC=(Button)findViewById(R.id.btn_M_C);
+        TipoConversion=MC.getText().toString();
+        Conversion="5";
+        CambiarVentana();
+    }
+    public void C_M(View v){
+        CM=(Button)findViewById(R.id.btn_C_M);
+        TipoConversion=CM.getText().toString();
+        Conversion="6";
+        CambiarVentana();
+    }
+    public void C_P(View v){
+        CP=(Button)findViewById(R.id.btn_C_INCH);
+        TipoConversion=CP.getText().toString();
+        Conversion="7";
+        CambiarVentana();
+    }
+    public void P_C(View v){
+        PC=(Button)findViewById(R.id.btn_INCH_C);
+        TipoConversion=PC.getText().toString();
+        Conversion="8";
         CambiarVentana();
     }
     public void CambiarVentana(){
         Intent Ventana=new Intent(getApplicationContext(),Conversion.class);
         Ventana.putExtra("Tipo_Conversion",TipoConversion);
+        Ventana.putExtra("Conver",Conversion);
         startActivity(Ventana);
     }
 }
